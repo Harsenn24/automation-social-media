675bf3258aaa5e121086c9120bd9ab522777877c
Harsenn Sangkakala <caesarharsenn@gmail.com>
2024-10-21 11:44:30 +0700

    testing push

--- helpers/query-find-user.helper.js
+++ helpers/query-find-user.helper.js
@@ -1,12 +1,15 @@
 const { QueryTypes } = require("sequelize");
 const sequelize = require("../config/index");
 
+const offsetLimit = `LIMIT 1000 OFFSET 256`
+
 async function queryFindUser() {
     const findUsers = await sequelize.query(
         `
         SELECT u.user_id 
-        FROM users u 
-        WHERE u.active = 1;
+FROM users u 
+WHERE u.active = 1;
+
         `,
         {
             type: QueryTypes.SELECT,
